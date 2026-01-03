b73b0ebb19c6988d254c1e595aa233093b2e65ab
Crementif <26669564+Crementif@users.noreply.github.com>
2026-01-03 06:26:22 +0100

    Bump version in update checker

--- src/utils/update_checker.cpp
+++ src/utils/update_checker.cpp
@@ -11,7 +11,7 @@
 #pragma comment(lib, "winhttp.lib")
 
 namespace UpdateChecker {
-    const std::string CURRENT_VERSION = "0.9.3";
+    const std::string CURRENT_VERSION = "0.9.4";
 
     static std::string TrimPrefixV(std::string v) {
         if (!v.empty() && (v[0] == 'v' || v[0] == 'V')) {
